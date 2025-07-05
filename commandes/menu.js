66b76befd6fef0a01f023f91b4f0833264b744f3
Qartey <qartmdbot@gmail.com>
2025-07-06 02:09:58 +0300

    Initial commit

---
+++ commandes/menu.js
@@ -0,0 +1,81 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+    moment.tz.setDefault('Etc/GMT');
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+  let infoMsg =  `
+╭━═「 *${s.BOT}* 」═━❂
+┃⊛╭───────────➻
+┃⊛│◆ 𝙾𝚠𝚗𝚎𝚛 : ${s.OWNER_NAME}
+┃⊛│◆ 𝙿𝚛𝚎𝚏𝚒𝚡 : [ ${s.PREFIXE} ] 
+┃⊛│◆ 𝙼𝚘𝚍𝚎 : *${mode}*
+┃⊛│◆ 𝚁𝚊𝚖  : 𝟴/𝟭𝟯𝟮 𝗚𝗕
+┃⊛│◆ 𝙳𝚊𝚝𝚎  : *${date}* 
+┃⊛│◆ 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
+┃⊛│◆ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : ᴍᴀᴛᴇʟᴇᴇ
+┃⊛│◆ 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜 : ${cm.length}
+┃⊛│◆ 𝚃𝚑𝚎𝚖𝚎 : 𝗠𝗔𝗧𝗘𝗟𝗘𝗘
+┃⊛└───────────➻
+╰─━━━━══─⊷─══━━━❂\n${readmore}`;
+    let menuMsg = `𝗠𝗔𝗧𝗘𝗟𝗘𝗘 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝗯𝗼𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀`;
+    for (const cat in coms) {
+        menuMsg += `
+╭━━═✺ *${cat}* ✺═━━⊷ 
+┊│┌═━━⊷•∞•⊷━━─⊛
+┊│┊
+┌┤┊ `;for (const cmd of coms[cat]) {
+          menuMsg += `          
+┊│┊☆  *${cmd}*`    
+        } 
+        menuMsg +=`
+┊│└═━━⊷•∞•⊷━━─⊛  
+╰━━━═⊷✺•∞•✺⊷═━━━⊷`
+    }
+    menuMsg += `
+> 𝗠𝗔𝗧𝗘𝗟𝗘𝗘 𝗺𝗮𝘀𝘁𝗲𝗿 𝗼𝗳 𝗱𝗶𝗴𝗶𝘁𝗮𝗹 𝗵𝗮𝗰𝗸𝘀\n
+`;
+   var lien = mybotpic();
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Luckymd*, déveloper Fredie Tech" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+       console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Luckymd*, déveloper Fredie Tech" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    repondre(infoMsg + menuMsg);
+}
+});
---
+++ commandes/repo.js
@@ -0,0 +1,84 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+
+zokou({ nomCom: "repo", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+      *𝚳𝚫𝚻𝚵𝐋𝚵𝚵-𝚻𝚳𝐃1 𝚰𝚳𝚸𝚯𝚪𝚻𝚫𝚴𝚻 𝚰𝚴𝐅𝚯* 
+❒───────────────────❒
+*GITHUB LINK*
+> https://github.com/tryphonee/MTALEE-TMD1
+
+*WHATSAPP CHANNEL*
+> https://whatsapp.com/channel/0029VajweHxKQuJP6qnjLM31
+⁠
+╭───────────────────❒
+│❒⁠⁠⁠⁠ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+│❒⁠⁠⁠⁠ *DEV1* : *timnasa tech*
+│❒⁠⁠⁠⁠ *DEV2* : *matelee tech*
+⁠⁠⁠⁠╰───────────────────❒
+  `;
+    
+let menuMsg = `
+     *𝗠𝗔𝗧𝗘𝗟𝗘𝗘-𝗺𝗮𝘀𝘁𝗲𝗿 𝗼𝗳 𝗱𝗶𝗴𝗶𝘁𝗮𝗹 𝗵𝗮𝗰𝗸𝘀*
+
+❒────────────────────❒`;
+
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+}); 
