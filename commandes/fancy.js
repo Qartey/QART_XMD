a5e26d561498d3c78b15bcfa67ee7d54568bb08a
Qartey <qartmdbot@gmail.com>
2025-07-06 09:31:15 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
 const fancy = require("../commandes/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "💢" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "✍️" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 QART XMD\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('*QART MD*', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 QART_XMD 2025\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('QART_XMD 2025', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
